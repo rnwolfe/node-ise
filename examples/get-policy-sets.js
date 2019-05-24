4e2b6cfefb7a9b6d4574bf1d4895ace40bbfc6ec
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-24 14:44:50 -0500

    getPolicySetes, refactor for _addQPH()

---
+++ examples/get-policy-sets.js
@@ -0,0 +1,9 @@
+const ISE = require('../');
+
+const ise = new ISE(process.env.ISE_HOST, process.env.ISE_USER, process.env.ISE_PASS);
+
+ise
+  .iseLogin()
+  .then(() => ise.getRadiusPolicySets())
+  .then(sets => console.dir(sets))
+  .catch(error => console.dir(error));
