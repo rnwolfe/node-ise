b46bda08d00732b54ddd380d77d156b52d631f64
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-29 15:25:31 -0400

    tcNacLiveLogs

---
+++ examples/get-tcnac-live-logs.js
@@ -0,0 +1,10 @@
+const ISE = require('..');
+
+const ise = new ISE(process.env.ISE_HOST, process.env.ISE_USER, process.env.ISE_PASS);
+
+ise
+  .login()
+  .then(() => ise.getTcNacLiveLogs())
+  .then(logs => console.dir(logs))
+  .then(() => ise.logout())
+  .catch(error => console.dir(error));
