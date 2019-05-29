bcbbb649c37b380934089042b2570e25260cd751
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-29 15:27:57 -0400

    radius live log counters

--- examples/get-radius-live-logs.js
+++ examples/get-radius-live-logs.js
@@ -6,5 +6,7 @@ ise
   .login()
   .then(() => ise.getRadiusLiveLogs())
   .then(logs => console.dir(logs))
+  .then(() => ise.getRadiusLiveLogCounters())
+  .then(counters => console.dir(counters))
   .then(() => ise.logout())
   .catch(error => console.dir(error));
