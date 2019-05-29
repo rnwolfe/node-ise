150a58a0db4762755790481c378d1dd2c5089fa0
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-29 14:43:14 -0400

    better defaults, app/nad endpoints, paths import

---
+++ examples/get-all-apps.js
@@ -0,0 +1,10 @@
+const ISE = require('../');
+
+const ise = new ISE(process.env.ISE_HOST, process.env.ISE_USER, process.env.ISE_PASS);
+
+ise
+  .login()
+  .then(() => ise.getApplications({ categories: 'VPNClient' }))
+  .then(apps => console.dir(apps))
+  .then(() => ise.logout())
+  .catch(error => console.dir(error));
---
+++ examples/get-all-nads.js
@@ -0,0 +1,11 @@
+const ISE = require('../');
+
+const ise = new ISE(process.env.ISE_HOST, process.env.ISE_USER, process.env.ISE_PASS);
+
+ise
+  .login()
+  .then(() => ise.getNetworkDevices())
+  // .then(() => ise.getNetworkDevices({ 'Device Type': 'Device Type#All Device Types#Switches' }))
+  .then(nads => console.dir(nads))
+  .then(() => ise.logout())
+  .catch(error => console.dir(error));
