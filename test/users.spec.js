c3e0f50d444f6542350817ff9e20b4d38d453701
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-30 19:05:19 -0400

    user tests

---
+++ test/users.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+require('./setup');
+const fs = require('fs');
+// if (!LIVE_TEST) require('./mocks/endpoints.nock');
+
+describe('User:', () => {
+  describe('Get users:', () => {
+    it('should get all users (no pagination)', async () => {
+      const users = await ise.getUsers();
+      expect(users)
+        .to.be.an('array')
+        .with.lengthOf.at.least(2);
+      expect(users[0])
+        .to.be.an('object')
+        .that.includes.keys(['username', 'NoOfDevicesPerUser']);
+    });
+    it('should get all users (with pagination)', async () => {
+      const users = await ise.getUsers({}, { pageSize: 1 });
+      expect(users)
+        .to.be.an('array')
+        .with.a.lengthOf.at.least(2);
+    });
+    it('should get all users (with filter)', async () => {
+      const users = await ise.getUsers({ userName: 'win10' });
+      expect(users)
+        .to.be.an('array')
+        .with.a.lengthOf.at.least(1);
+    });
+  });
+});
