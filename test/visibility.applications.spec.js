5e262e2365834eae52449d0fa37e27fc8271beb5
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-31 10:12:59 -0400

    add app coverage, rename some tests to ensure auth runs first

---
+++ test/visibility.applications.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+require('./setup');
+// if (!LIVE_TEST) require('./mocks/endpoints.nock');
+
+// Current test environment has no apps so returns empty data structure.
+const APP_KEYS = [];
+
+describe('Applications:', () => {
+  describe('Get applications:', () => {
+    it('should get all applications (no pagination)', async () => {
+      const apps = await ise.getApplications();
+      expect(apps)
+        .to.be.an('array')
+        .with.lengthOf.at.least(0);
+      // expect(apps[0])
+      //   .to.be.an('object')
+      //   .that.includes.keys(APP_KEYS);
+    });
+    it('should get all applications (with pagination)', async () => {
+      const apps = await ise.getApplications({}, { pageSize: 1 });
+      expect(apps)
+        .to.be.an('array')
+        .with.a.lengthOf.at.least(0);
+    });
+    it('should get all applications (with filter)', async () => {
+      const apps = await ise.getApplications({ type: 'VPNClient' });
+      expect(apps)
+        .to.be.an('array')
+        .with.a.lengthOf.at.least(0);
+    });
+    // it('should get a single user', async () => {
+    //   const app = await ise.getApplication('appname');
+    //   expect(app)
+    //     .to.be.an('object')
+    //     .that.includes.keys(APP_KEYS);
+    // });
+  });
+});
