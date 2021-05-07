5e4e7290236a863b615105f839aa470f7a493782
Ryan Wolfe <rn.wolfe@gmail.com>
2021-05-07 15:41:37 -0400

    get profile policies, faster lookup for ep create

--- test/endpoint.identitygroups.spec.js
+++ test/endpoint.identitygroups.spec.js
@@ -14,11 +14,13 @@ const ENDPOINT_IDENTITY_GROUP_KEYS = [
   'rbacPermission'
 ];
 
+const GROUP_DROPDOWN_KEYS = ['id', 'name', 'label'];
+
 describe('Endpoint Identity Groups:', () => {
   describe('Get endpoint identity groups:', () => {
     it('should get at least 15 identity groups', async () => {
       const groups = await ise.getEndpointIdentityGroups();
-      expect(groups).to.be.an('array').with.lengthOf.at.least(15).and.eachOf;
+      expect(groups).to.be.an('array').with.lengthOf.at.least(15);
       expect(groups[0])
         .to.be.an('object')
         .that.includes.keys(ENDPOINT_IDENTITY_GROUP_KEYS);
@@ -28,6 +30,20 @@ describe('Endpoint Identity Groups:', () => {
       expect(group)
         .to.be.an('object')
         .that.includes.keys(ENDPOINT_IDENTITY_GROUP_KEYS)
+        .and.includes({ name: 'Blacklist' });
+    });
+    it('should get identity groups for dropdown list', async () => {
+      const groups = await ise.getEndpointIdentityGroupList();
+      expect(groups).to.be.an('array').with.lengthOf.at.least(15);
+      expect(groups[0])
+        .to.be.an('object')
+        .that.includes.keys(GROUP_DROPDOWN_KEYS);
+    });
+    it('should get single identity group for dropdown list', async () => {
+      const group = await ise.getEndpointIdentityGroupList('Blacklist');
+      expect(group)
+        .to.be.an('object')
+        .that.includes.keys(GROUP_DROPDOWN_KEYS)
         .and.includes({ name: 'Blacklist' });
     });
   });
