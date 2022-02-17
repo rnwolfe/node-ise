bbe163a67f737978c66f145cb4be2d72343cc3f8
Ryan Wolfe <rn.wolfe@gmail.com>
2022-02-17 13:19:55 -0500

    fix: :construction: Tweak profiling retrieval and endpoint creation, Update code formatting

---
+++ examples/profiler/get-endpoint-profile-policies.js
@@ -0,0 +1,14 @@
+const ise = require('../setup');
+
+ise
+  .login()
+  .then(() => ise.getEndpointProfilingPolicies(0, 1500))
+  .then((profiles) => {
+    console.dir(profiles);
+    console.log(`There are ${profiles.totalItems} profiles.`);
+    return profiles.items.map((profile) => profile.name);
+  })
+  .then((list) =>
+    console.log('Here is a quick list', list, '\n Count: ', list.length)
+  )
+  .catch((error) => console.dir(error));
