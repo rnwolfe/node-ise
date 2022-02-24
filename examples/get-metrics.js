ffd09fef2592620932d9e2e23a7dcbe7cddca539
Ryan Wolfe <rn.wolfe@gmail.com>
2022-02-24 11:14:42 -0500

    feat: :sparkles: Add getTotalEndpoints from metric data

--- examples/get-metrics.js
+++ examples/get-metrics.js
@@ -8,14 +8,16 @@ ise
       ise.getRejectedEndpoints(),
       ise.getAnomalousEndpoints(),
       ise.getByodEndpoints(),
-      ise.getAuthenticatedGuest()
+      ise.getAuthenticatedGuest(),
+      ise.getTotalEndpoints()
     ]).then((results) => {
       return {
         activeEndpoints: results[0],
         rejectedEndpoints: results[1],
         anomalousEndpoints: results[2],
         byodEndpoints: results[3],
-        authenticatedGuest: results[4]
+        authenticatedGuest: results[4],
+        totalEndpoints: results[5]
       };
     })
   )
