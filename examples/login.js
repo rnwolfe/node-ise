94b595e15160962aaa43d474c2287ff2630ee385
Ryan Wolfe <rn.wolfe@gmail.com>
2022-05-13 12:21:07 -0400

    feat: :sparkles: Fix authentication for ISE 3.1
    This may break authentications to previous versions, if so, pin your package.json to a previous commit.

--- examples/login.js
+++ examples/login.js
@@ -3,10 +3,10 @@ const ise = require('./setup');
 ise
   .login()
   .then(() => {
-    console.log(
-      'Logged in! ISE is running version',
-      ise.iseVersion,
-      ise.csrfToken
-    );
+    console.log(`Logged in: ${ise.isLoggedIn}!`);
+    console.log(`ISE is running version: ${ise.iseVersion}`);
+    console.log(`ISE is running on: ${ise.host}`);
+    console.log(`ISE session token: ${ise.sessionId}`);
+    console.log(`ISE CSRF token: ${ise.csrfToken || 'N/A'}`);
   })
   .catch(error => console.log(error));
