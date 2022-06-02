2e6eea2a7a3306753a517743aa2abc56653c7e72
Ryan Wolfe <rn.wolfe@gmail.com>
2022-06-02 14:31:02 -0400

    feat: :sparkles: Allow Bottleneck options passed into contstructor

--- examples/setup.js
+++ examples/setup.js
@@ -8,6 +8,7 @@ const ISE = require('..');
 const ise = new ISE(
   process.env.ISE_HOST,
   process.env.ISE_USER,
-  process.env.ISE_PASS
+  process.env.ISE_PASS,
+  { maxConcurrent: 1, minTime: 200 }
 );
 module.exports = ise;
